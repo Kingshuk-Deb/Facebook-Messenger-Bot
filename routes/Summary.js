2a3002d3e06babd45a867453fe2b8be92a201ae9
Kingshuk Deb <74722462+Kingshuk-Deb@users.noreply.github.com>
2022-02-17 12:40:40 +0530

    Update Summary.js

--- routes/Summary.js
+++ routes/Summary.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const summaryController = require('../controllers/summary');
 
-router.get('/', summaryController.get_Summary);
+router.get('/', summaryController.get_Summary); // Route Function to Get All User Datas
 
 module.exports = router;
