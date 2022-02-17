b797fc3330846657b0ab2dc2d97004a5aa0922a2
Kingshuk Deb <ronnie@Kingshuks-MacBook-Air.local>
2022-02-17 13:26:40 +0530

    Updating Try-Catch

--- controllers/summary.js
+++ controllers/summary.js
@@ -3,16 +3,18 @@ const Messages = require('../models/messagesDB');
 
 exports.get_Summary = async (req, res) => {
     // Queries All Users & Messages From Mongo Collection And Pushes Them in 'summary' Array
-    let summary = []; const users = await Users.find({}, { _id: 0 });
-    users.map( async (element) => {
-        let msg = []; const messages = await Messages.find({user_id: element.user}, { _id: 0 });
-        messages.map((ele, index) => { msg.push(ele.message) })
-        let userObj = {
-            user: element.user, 
-            name: element.name, 
-            messages: msg
-        }
-        summary.push(userObj);
-    });
-    setTimeout(() => { res.json({result: summary}); }, 100);
+    try {
+        let summary = []; const users = await Users.find({}, { _id: 0 });
+        users.map( async (element) => {
+            let msg = []; const messages = await Messages.find({user_id: element.user}, { _id: 0 });
+            messages.map((ele, index) => { msg.push(ele.message) })
+            let userObj = {
+                user: element.user, 
+                name: element.name, 
+                messages: msg
+            }
+            summary.push(userObj);
+        });
+        setTimeout(() => { res.json({result: summary}); }, 100);
+    } catch (e) { res.json({success: false}); }
 }
