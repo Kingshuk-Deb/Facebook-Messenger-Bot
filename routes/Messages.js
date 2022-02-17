10760f0c9d3c291bd982f2aaa26fe4f36eb0b46c
Kingshuk Deb <74722462+Kingshuk-Deb@users.noreply.github.com>
2022-02-17 12:40:21 +0530

    Update Messages.js

--- routes/Messages.js
+++ routes/Messages.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messages');
 
-router.get('/', messageController.get_all_messages);
-router.get('/:id', messageController.get_message_byID);
+router.get('/', messageController.get_all_messages); // Route Function To Get All Messages
+router.get('/:id', messageController.get_message_byID); // Route Function To Get Message by Message ID
 
 module.exports = router;
