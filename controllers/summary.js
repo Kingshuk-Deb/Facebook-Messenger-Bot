588811374c9651633d34c13bb7a3d9d60560f297
Kingshuk Deb <ronnie@Kingshuks-MacBook-Air.local>
2022-02-18 13:06:11 +0530

    FIX Issue #1 - await update

--- controllers/summary.js
+++ controllers/summary.js
@@ -7,18 +7,15 @@ exports.get_Summary = async (req, res) => {
         let summary = []; const users = await Users.find({}, { _id: 0 });
         const messages = await Messages.find({}, { _id: 0 });
         users.map((element, index) => {
-            let msg = [];
-            messages.map((ele, i) => {
-                if(ele.user_id === element.user) msg.push(ele.message)
-                if(messages.length - 1 === i) { 
-                    let userObj = {
-                        user: element.user, 
-                        name: element.name, 
-                        messages: msg
-                    }
-                    summary.push(userObj); 
-                }
-            });
+            let msg = messages.map((ele, i) => {
+                if(ele.user_id === element.user) return ele.message;
+            }).filter(notUndefined => notUndefined !== undefined);
+            let userObj = {
+                user: element.user, 
+                name: element.name, 
+                messages: msg
+            }
+            summary.push(userObj); 
             if(users.length - 1 === index) {
                 res.json({result: summary});
             }
