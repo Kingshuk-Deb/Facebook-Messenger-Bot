cd81080662ed117c783fdd2f12b3c616be1340cb
Kingshuk Deb <ronnie@Kingshuks-MacBook-Air.local>
2022-02-18 12:47:32 +0530

    FIX Issue #1 - await

--- controllers/summary.js
+++ controllers/summary.js
@@ -5,15 +5,20 @@ exports.get_Summary = async (req, res) => {
     // Queries All Users & Messages From Mongo Collection And Pushes Them in 'summary' Array
     try {
         let summary = []; const users = await Users.find({}, { _id: 0 });
-        users.map( async (element, index) => {
-            let msg = []; const messages = await Messages.find({user_id: element.user}, { _id: 0 });
-            messages.map(async (ele) => { msg.push(ele.message) })
-            let userObj = {
-                user: element.user, 
-                name: element.name, 
-                messages: msg
-            }
-            summary.push(userObj);
+        const messages = await Messages.find({}, { _id: 0 });
+        users.map((element, index) => {
+            let msg = [];
+            messages.map((ele, i) => {
+                if(ele.user_id === element.user) msg.push(ele.message)
+                if(messages.length - 1 === i) { 
+                    let userObj = {
+                        user: element.user, 
+                        name: element.name, 
+                        messages: msg
+                    }
+                    summary.push(userObj); 
+                }
+            });
             if(users.length - 1 === index) {
                 res.json({result: summary});
             }
